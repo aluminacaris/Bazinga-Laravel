caae2507b5ca7462cfe0aec1d17aba3c785189b4
marcosmonteirojr <marcos.monteirojr@gmail.com>
2025-11-19 22:39:02 -0300

    19/11/2025

--- resources/views/category/categoryIndex.blade.php
+++ resources/views/category/categoryIndex.blade.php
@@ -1,7 +1,31 @@
 @extends('layout')
 @section('content')
+<div class="container">
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    @foreach($categories as $cat)
+    <div class="col">
+      <div class="card shadow-sm"> <img src= "storage/app/public/image/categories/download.png"></img>
+     <div class="card-body">
+          <p class="card-text">{{$cat->name}}</p>
+          <p class="card-text">{{$cat->descritpion}}</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group"> <button type="button" class="btn btn-sm btn-outline-secondary">View</button> <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> </div> <small class="text-body-secondary">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+      @endforeach
+    </div>
+  </div>
+
+
+
+
+
+
+
 <table class="table table-dark table-striped">
-   <thead>
+  <thead>
     <tr>
       <th scope="col">id</th>
       <th scope="col">Nome</th>
@@ -9,8 +33,8 @@
       <th scope="col">Editar</th>
       <th scope="col">Mostrar</th>
     </tr>
-    <tbody>
-        @foreach($categories as $cat)
+  <tbody>
+    @foreach($categories as $cat)
     <tr>
       <th scope="row">{{$cat->id}}</th>
       <td>{{$cat->name}}</td>
@@ -19,7 +43,7 @@
       <td><a href="{{route('category.show', $cat->id)}}"><button type="button" class="btn btn-success" hres>Mostrar</button></td>
     </tr>
     @endforeach
-    </tbody>
+  </tbody>
   </thead>
 </table>
 @endsection
